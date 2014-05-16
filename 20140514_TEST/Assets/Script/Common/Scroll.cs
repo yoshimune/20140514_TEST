2e667544c108e60c080b1adf65f6bee5a37258d9
yoshimune <seatangle.dash@gmail.com>
2014-05-16 19:24:09 +0900

    スクロール追加

---
+++ 20140514_TEST/Assets/Script/Common/Scroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 vec = new Vector3(transform.position.x +0.1f, transform.position.y, transform.position.z);
+		transform.position = vec;
+
+		if(transform.position.x > 10.0f){
+			vec = new Vector3 (-10.0f, transform.position.y, transform.position.z);
+			transform.position = vec;
+		}
+	}
+}
